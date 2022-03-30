b7551ea7fb985d7d20519c907ffd3a73ef8c112b
samuelgoh1525 <samuelgoh1525@gmail.com>
2022-03-30 03:43:23 -0400

    Started on landing page
    - Added fontawesome
    - Tried using Bulma with react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 'bulma/css/bulma.min.css';
+// import your fontawesome library
+import './fontAwesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 function App() {
+  return (
+    <div classname="main">
+      <section class="hero is-dark is-fullheight-with-navbar" style={{backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.7)), url(https://img.wallpapersafari.com/desktop/1920/1080/16/58/Aymbd9.jpg)', backgroundSize: 'cover'}}>
+        <div class="hero-body">
+          <div class="container has-text-left">
+            <h1 class="title is-1 is-spaced">
+              <FontAwesomeIcon icon="fa-solid fa-person-running" />
+              WeFit
+            </h1>
+            <h2 class="subtitle">Friends that sweat together stay together</h2>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+  /*
   return (
     <div className="App">
       <header className="App-header">
@@ -15,11 +36,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello
         </a>
       </header>
     </div>
   );
+  */
 }
 
 export default App;
---
+++ src/fontAwesome.js
@@ -0,0 +1,10 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+import { faPersonRunning } from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faPersonRunning,
+  // more icons go here
+);
