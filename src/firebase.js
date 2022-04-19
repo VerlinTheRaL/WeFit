7e0c76dd9ee14ad91872f88f6ec55492dc69b461
徐远航 <somnus@Dante.local>
2022-04-18 20:18:37 -0400

    Transfer from web version 8 to 9

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-import Firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-
 import { seedDatabase } from "./seed";
 
 // wefit config
@@ -23,9 +19,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const firebase = Firebase.initializeApp(firebaseConfig);
-const { FieldValue } = Firebase.firestore;
 
 // This only needs to be ran once to manually add some test users to db
 // seedDatabase(db);
-export { app, auth, db, firebase, FieldValue };
+export { app, auth, db };
